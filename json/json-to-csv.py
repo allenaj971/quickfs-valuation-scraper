fdeeb4267709465b44e9f960d3c7089214e3eaf3
sciencetechrocks <sciencetechrocks@gmail.com>
2021-02-21 15:22:30 +1030

    edited readme.md

--- json/json-to-csv.py
+++ json/json-to-csv.py
@@ -1,11 +1,11 @@
-# Json-to-csv converter if you needusing pandas because pandas is 🔥🔥🔥
+# Json-to-csv converter if you need using pandas because pandas is 🔥🔥🔥
 import pandas
 
 # https://stackoverflow.com/questions/1871524/how-can-i-convert-json-to-csv
-cfs = pandas.read_json('./cash_flow_statement.json')
-bs = pandas.read_json('./balance_sheet.json')
-income_s = pandas.read_json('./income_statement.json')
+cfs = pandas.read_json('./scraped_financials/cash_flow_statement.json')
+bs = pandas.read_json('./scraped_financials/balance_sheet.json')
+income_s = pandas.read_json('./scraped_financials/income_statement.json')
 
-cfs.to_csv('./cash_flow_statement.csv')
-bs.to_csv('./balance_sheet.csv')
-income_s.to_csv('./income_statement.csv')
+cfs.to_csv('./scraped_financials/cash_flow_statement.csv')
+bs.to_csv('./scraped_financials/balance_sheet.csv')
+income_s.to_csv('./scraped_financials/income_statement.csv')
